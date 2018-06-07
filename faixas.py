6c392560aa8daf7ba97d8c4c20755a3b5a7f3ffb
Kayan Almeida <kayant.almeida@gmail.com>
2018-06-07 17:49:33 -0300

    Last digit bugfix

--- faixas.py
+++ faixas.py
@@ -26,7 +26,7 @@ def update_count_json(content):
 
 
 def generate_file(mes, ano, tipo, quantidade, municipio):
-    increment = 11
+    digito = 0
     cod_rio = '33'
     counter = 0
     line = '';
@@ -35,25 +35,43 @@ def generate_file(mes, ano, tipo, quantidade, municipio):
     file_str = gerar_cabecalho(
         mes, ano, procedimento["nome"], quantidade, municipio, numero_municipio)
     final_ano = ano[-2:]
-    prev_last = procedimento["last"] + increment
-    new_last = prev_last + quantidade * increment
+    prev_last = procedimento["last"]
+    new_last = prev_last + quantidade
     procedimento["last"] = new_last
-    update_count_json(procedimentos)
-    for i in range(prev_last, new_last, increment):
+    last_digit = procedimento["last_digit"]
+    if (last_digit == 10):
+        last_digit = -1
+    for i in range(prev_last + 1, new_last):
+        last_digit += 1
         if counter == 0:
             line = '"'
         counter += 1
-        n = '%08d' % i
-        n = n[:-1] + '-' + n[-1]
+        n = '%07d' % i
+        last_digit = last_digit % 11
+        n = n + '-'
+        n += '%d' % (last_digit % 10)
         number = cod_rio + final_ano + procedimento["number"] + n
         line += number
-        if counter < 4 and i != new_last - 11:
+        if counter < 4 and i != new_last - 1:
             line += "  !  "
         else:
             counter = 0
             line += ' "\n'
             file_str += line
     write_file(file_str, procedimento["nome"], municipio, mes, ano)
+    procedimento["last_digit"] = last_digit
+    update_count_json(procedimentos)
 
 
-generate_file(5, '2018','apac', 24998, u'RIO DE JANEIRO')
+# AIH 331810-0
+# AIH ELETIVA 331850029795-9
+# APAC 331820325548-8
+# APAC ELETIVA 331860009030-0
+
+generate_file(5, '2018','apac', 50000, u'GERAL')
+
+# generate_file(5, '2018','apac_e', 50000, u'GERAL')
+
+# generate_file(5, '2018','aih_e', 50000, u'GERAL')
+
+# generate_file(5, '2018','aih', 50000, u'GERAL')
